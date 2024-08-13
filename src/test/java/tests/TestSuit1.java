a48185de0ae697acc4bd1c42e9aa053e282e3ecf
Gayatri-7299 <gayatrikannewar99@gmail.com>
2024-08-13 10:32:10 +0530

    Initial Commit

---
+++ src/test/java/tests/TestSuit1.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Test;
+
+public class TestSuit1 {
+
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Test2");
+	}
+}
---
+++ src/test/java/tests/TestSuit2.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Test;
+
+public class TestSuit2 {
+
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Test2");
+	}
+}
